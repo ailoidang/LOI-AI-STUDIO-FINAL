e989ba033603b44b299d0adfaaabb73f3f018192
ailoidang <ai.loidang@gmail.com>
2026-02-04 15:43:49 +0700

    Change base path for production deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        base: '/loiaistudio/',
+        base: '/LOI-AI-STUDIO-FINAL/',
       server: {
         port: 3000,
         host: '0.0.0.0',
